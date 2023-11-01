4889f62f663800405dd91d3e1883756979a617cf
Yash Chauhan <yash.chauhan062@nmims.edu.in>
2023-11-02 00:27:29 +0530

    Gauge single initialization in main

--- promethous_azure.py
+++ promethous_azure.py
@@ -30,23 +30,25 @@ def get_metric_data(resource_id, credential, subscription_id):
 
     return metrics_data.as_dict()
 
-def collect_metrics(resource_id, credential, subscription_id):
-    azure_metric = Gauge('azure_percentage_cpu', 'Percentage CPU used by the Azure resource')
+def collect_metrics(azure_metric, resource_id, credential, subscription_id):
     # Collect metrics from Azure (use the previous snippet)
     # ...
     metric_value = get_metric_data(resource_id, credential, subscription_id)['value'][0]['timeseries'][0]['data'] # Assume this is the metric value you've collected from Azure
     for i in metric_value:
         if 'total' in i.keys():
+            print(i, end=' ')
             azure_metric.set(i['total'])
+    print()
 
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
+    azure_metric = Gauge('azure_percentage_cpu', 'Percentage CPU used by the Azure resource')
     start_http_server(8000)
     resource_id="/subscriptions/94ba27d6-505e-44e4-ae1d-0a39c5296f06/resourcegroups/cerebrone-interns/providers/Microsoft.Compute/virtualMachines/yash-vm-trial"
     while True:
         print('Collecting metrics...')
-        collect_metrics(resource_id, get_default_credential(), config.SUBSCRIPTION_ID)
+        collect_metrics(azure_metric, resource_id, get_default_credential(), config.SUBSCRIPTION_ID)
         print('Sleeping...')
         time.sleep(60)
     # pprint.pprint(get_metric_data(resource_id, get_default_credential(), config.SUBSCRIPTION_ID)['value'][0]['timeseries'][0]['data'])
